e21e0bd369faf54f6bd11ea2ad6a7ed5ff5dfbd6
valeriydutt <39168826+valeriydutt@users.noreply.github.com>
2021-02-17 13:42:18 +0000

    added .gitignore

---
+++ dataset_mean_and_std.py
@@ -0,0 +1,36 @@
+import torch
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+import torchvision.datasets as datasets
+from tqdm import tqdm
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# datasets.ImageFolder can be replaced with any popular dataset, e.g.
+#train_set = datasets.CIFAR10(root="ds/", transform=transforms.ToTensor(), download=True)
+train_set = datasets.ImageFolder(root="./train/", transform=transforms.Compose([
+    transforms.Scale(256),
+    transforms.RandomCrop(224),
+    transforms.ToTensor()
+]))
+train_loader = DataLoader(dataset=train_set, batch_size=64, shuffle=True)
+
+def get_mean_std(loader):
+    # var[X] = E[X**2] - E[X]**2
+    channels_sum, channels_sqrd_sum, num_batches = 0, 0, 0
+
+    for data, _ in tqdm(loader):
+        channels_sum += torch.mean(data, dim=[0, 2, 3])
+        channels_sqrd_sum += torch.mean(data ** 2, dim=[0, 2, 3])
+        num_batches += 1
+
+    mean = channels_sum / num_batches
+    std = (channels_sqrd_sum / num_batches - mean ** 2) ** 0.5
+
+    return mean, std
+
+
+mean, std = get_mean_std(train_loader)
+print(mean)
+print(std)

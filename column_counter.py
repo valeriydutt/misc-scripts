7678930264e6eb7a541a712b7d7ba38589b335e5
valeriydutt <39168826+valeriydutt@users.noreply.github.com>
2021-02-20 08:59:06 +0000

    added seaborn visualization

--- column_counter.py
+++ column_counter.py
@@ -1,5 +1,9 @@
 import csv
 import collections
+import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
+sns.set(color_codes=True)
 
 diagnosis = collections.Counter()
 with open('train/train.csv') as input_file:
@@ -8,3 +12,12 @@ with open('train/train.csv') as input_file:
 
 print('Number of severe cases (4): %s' % diagnosis['4'])
 print(diagnosis.most_common())
+
+dg = pd.read_csv('train/train.csv')
+ax = sns.countplot(x=dg['diagnosis'])
+ax.set_title('Severity of diabetic Retinopathy')
+ax.set_ylabel('Count')
+ax.set(xlabel=None)
+
+dg.info()
+plt.show()

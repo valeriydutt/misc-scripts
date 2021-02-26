3007f71ba967807267e6df983b27ff52c56cfdb4
valeriydutt <39168826+valeriydutt@users.noreply.github.com>
2021-02-26 12:36:12 +0000

    show percentage on the bar graph

--- column_counter.py
+++ column_counter.py
@@ -6,18 +6,26 @@ import matplotlib.pyplot as plt
 sns.set(color_codes=True)
 
 diagnosis = collections.Counter()
-with open('train/train.csv') as input_file:
+with open('train/train2.csv') as input_file:
     for row in csv.reader(input_file, delimiter=','):
         diagnosis[row[1]] += 1
 
 print('Number of severe cases (4): %s' % diagnosis['4'])
 print(diagnosis.most_common())
+dg = pd.read_csv('train/train2.csv')
 
-dg = pd.read_csv('train/train.csv')
-ax = sns.countplot(x=dg['diagnosis'])
-ax.set_title('Severity of diabetic Retinopathy')
+df = pd.DataFrame(dict(x=dg['diagnosis']))
+ax = sns.barplot(x="x", y="x", data=df, estimator=lambda x: (len(x) / len(df) *100))
+ax.set(ylabel="Percent")
+
+ax.set_title('Severity of Diabetic Retinopathy')
 ax.set_ylabel('Count')
 ax.set(xlabel=None)
 
+for p in ax.patches:
+    width = p.get_width()
+    height = p.get_height()
+    x, y = p.get_xy() 
+    ax.annotate(f'{height/100:.2%}', (x + width/2, y + height*1.02), ha='center')
 dg.info()
 plt.show()
